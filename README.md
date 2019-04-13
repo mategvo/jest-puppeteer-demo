89137e69b2a4b9d37390800cb6a842f4e04dcc39
Mateusz Gwóźdź <gwozdz.mateusz@gmail.com>
2019-04-13 11:22:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+This is a simple demo of jest + puppeteer e2e test. 
+
+# How to run the test
+
+Install dependencies
+
+`$ npm i` 
+
+Run test 
+
+`$ npm test`
+
+# expected results 
+
+The test should open chromium browser, google.com page, type in the input and verify text content of the first URL
---
+++ shop.spec.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer')
+
+test('first link should lead to angel.co profile', async () => {
+
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 1,
+    })
+    const page = await browser.newPage()
+    await page.goto('https://google.com')
+
+    await page.click('input.gLFyf.gsfi')
+    await page.type('input.gLFyf.gsfi', 'Mate Gwozdz')
+    await page.type('input.gLFyf.gsfi', String.fromCharCode(13));
+
+    await page.waitFor(1000);
+
+    const element = await page.$("a cite");
+    const text = await page.evaluate(element => element.textContent, element);
+
+    expect(text).toBe('https://angel.co/mategwozdz')
+
+
+}, 100000)
+
